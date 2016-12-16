278604dc321688da42c835e35ba858bf654aa000
mamingzhang <mamingzhang@shuzijiayuan.com>
2016-12-16 20:37:13 +0800

    UI显示
    TODO：ButterKnife引入解析View空指针，ToolBar和ListView

--- code/app/src/main/java/com/example/mamingzhang/androidstructuretest/activity/MainActivity.java
+++ code/app/src/main/java/com/example/mamingzhang/androidstructuretest/activity/MainActivity.java
@@ -2,15 +2,11 @@ package com.example.mamingzhang.androidstructuretest.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.mamingzhang.androidstructuretest.R;
 import com.example.mamingzhang.androidstructuretest.activity.base.BaseActivity;
-import com.example.mamingzhang.androidstructuretest.config.FragmentConfig;
+import com.example.mamingzhang.androidstructuretest.adapter.MainAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +21,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.setDebug(true);
         ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -33,48 +30,4 @@ public class MainActivity extends BaseActivity {
         listView.setAdapter(new MainAdapter());
     }
 
-    private class MainAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return FragmentConfig.ExampleFragment.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            Object[] keySet = FragmentConfig.ExampleFragment.keySet().toArray();
-            return keySet[position];
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View view, ViewGroup viewGroup) {
-            ViewHolder viewHolder;
-
-            if (view == null) {
-                view = getLayoutInflater().inflate(R.layout.listitem_main_activity, viewGroup, false);
-                viewHolder = new ViewHolder(view);
-                view.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) view.getTag();
-            }
-
-            viewHolder.txtView.setText(getItem(position).toString());
-
-            return view;
-        }
-
-        class ViewHolder {
-            @BindView(R.id.txtView)
-            TextView txtView;
-
-            ViewHolder(View view) {
-                ButterKnife.bind(this, view);
-            }
-        }
-    }
 }
---
+++ code/app/src/main/java/com/example/mamingzhang/androidstructuretest/adapter/MainAdapter.java
@@ -0,0 +1,61 @@
+package com.example.mamingzhang.androidstructuretest.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.mamingzhang.androidstructuretest.R;
+import com.example.mamingzhang.androidstructuretest.config.FragmentConfig;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by mamingzhang on 16/12/16.
+ */
+
+public class MainAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return FragmentConfig.ExampleFragment.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        Object[] keySet = FragmentConfig.ExampleFragment.keySet().toArray();
+        return keySet[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if (view == null) {
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listitem_main_activity, viewGroup, false);
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.txtView.setText(getItem(position).toString());
+
+        return view;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.txtView)
+        TextView txtView;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
