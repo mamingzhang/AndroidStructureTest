40f29fff9ab28d4c962a3aaebe8221550557b405
mamingzhang <mamingzhang@shuzijiayuan.com>
2016-12-16 17:44:52 +0800

    Add RxJava And So On

--- code/app/src/main/java/com/example/mamingzhang/androidstructuretest/activity/MainActivity.java
+++ code/app/src/main/java/com/example/mamingzhang/androidstructuretest/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.mamingzhang.androidstructuretest;
+package com.example.mamingzhang.androidstructuretest.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.mamingzhang.androidstructuretest.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ code/app/src/main/java/com/example/mamingzhang/androidstructuretest/data/manager/BaseDataManager.java
@@ -0,0 +1,8 @@
+package com.example.mamingzhang.androidstructuretest.data.manager;
+
+/**
+ * Created by mamingzhang on 16/12/16.
+ */
+
+public class BaseDataManager {
+}
