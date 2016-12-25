8e5df6540c0d5bd0e8fa704203295fbf93a39352
horsege <horse-tafeiyan@163.com>
2016-12-25 21:35:38 +0800

    Add Doc

--- code/app/src/main/java/com/example/mamingzhang/androidstructuretest/fragment/BasicLocalExampleFragment.java
+++ code/app/src/main/java/com/example/mamingzhang/androidstructuretest/fragment/BasicLocalExampleFragment.java
@@ -16,6 +16,12 @@ import static com.example.mamingzhang.androidstructuretest.data.http.HttpResultC
 
 /**
  * Created by horsege on 2016/12/25.
+ * <p>
+ * 相关文档参考资料：
+ * 1. https://github.com/dreamhead/moco/blob/master/moco-doc/apis.md
+ * 2. http://www.coderli.com/mock-server-moco-guide/
+ * 3. http://www.2cto.com/kf/201610/554333.html
+ * 4. http://sanjay-f.github.io/2015/12/07/Android%E6%B5%8B%E8%AF%95%E6%95%99%E7%A8%8B13--%E6%A8%A1%E6%8B%9F%E6%9C%8D%E5%8A%A1%E5%99%A8MockServer%E4%B9%8BMoco%E8%AF%A6%E7%BB%86%E4%BB%8B%E7%BB%8D/
  */
 
 public class BasicLocalExampleFragment extends BaseMovieDisplayFragment implements IHttpResultCodeWithCodeMsg<List<MovieSubject>> {
